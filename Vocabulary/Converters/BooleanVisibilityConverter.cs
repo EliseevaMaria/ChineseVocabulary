06e89415f86e7e14ec247e684057ade0ee9c650c
mariia.yelisieieva <mariia.yelisieieva@sigma.software>
2017-11-22 17:28:48 +0200

    Loading spinner added

---
+++ Vocabulary/Converters/BooleanVisibilityConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using Vocabulary.Model;
+
+namespace Vocabulary.Converters
+{
+    /// <summary>
+    /// Class for converting boolean values to UI elements' visible property.
+    /// </summary>
+    /// <owner>Mariia Yelisieieva</owner>
+    /// <seealso cref="System.Windows.Data.IValueConverter" />
+    public class BooleanVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts boolean values to the <see cref="Visibility"/> property, if <c>false</c> element is collapsed.
+        /// </summary>
+        /// <owner>Mariia Yelisieieva</owner>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isHidden = (bool) (value ?? false);
+
+            Visibility result = isHidden ? Visibility.Collapsed : Visibility.Visible;
+
+            return result;
+        }
+
+        /// <summary>
+        /// Converts the <see cref="Visibility"/> property to boolean values, if element is collapsed returns <c>false</c>. Throws <see cref="NotImplementedException" />.
+        /// </summary>
+        /// <owner>Mariia Yelisieieva</owner>
+        /// <param name="value">The value that is produced by the binding target.</param>
+        /// <param name="targetType">The type to convert to.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VocabularyUnitTest/Converters/BooleanVisibilityConverterTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Media;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vocabulary.Converters;
+using Vocabulary.Model;
+
+namespace VocabularyUnitTest.Converters
+{
+    /// <summary>
+    /// The class to test <see cref="BooleanVisibilityConverter"/>.
+    /// </summary>
+    /// <owner>Mariia Yelisieieva</owner>
+    [TestClass]
+    public class BooleanVisibilityConverterTest
+    {
+        /// <summary>
+        /// Checks whether false is converted to Visibility.Visible.
+        /// </summary>
+        /// <owner>Mariia Yelisieieva</owner>
+        [TestMethod]
+        public void BooleanVisibilityConverter_Convert_False_ReturnVisible()
+        {
+            BooleanVisibilityConverter converter = new BooleanVisibilityConverter();
+
+            Visibility result = (Visibility)converter.Convert(false, typeof(bool), new Object(), CultureInfo.CurrentCulture);
+
+            Assert.AreEqual(Visibility.Visible, result);
+        }
+
+        /// <summary>
+        /// Checks whether true is converted to Visibility.Collapsed.
+        /// </summary>
+        /// <owner>Mariia Yelisieieva</owner>
+        [TestMethod]
+        public void BooleanVisibilityConverter_Convert_True_ReturnCollapsed()
+        {
+            BooleanVisibilityConverter converter = new BooleanVisibilityConverter();
+
+            Visibility result = (Visibility)converter.Convert(true, typeof(bool), new Object(), CultureInfo.CurrentCulture);
+
+            Assert.AreEqual(Visibility.Collapsed, result);
+        }
+
+        /// <summary>
+        /// Checks whether the back conversion implemented.
+        /// </summary>
+        /// <owner>Mariia Yelisieieva</owner>
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void BooleanVisibilityConverter_ConvertBack_DefaultParameters_ThrowException()
+        {
+            ProgressColorConverter converter = new ProgressColorConverter();
+
+            converter.ConvertBack(new Object(), typeof(SolidColorBrush), new Object(), CultureInfo.CurrentCulture);
+        }
+    }
+}
